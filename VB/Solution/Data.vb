941d350aa9c414c953438496f2d714c5a89d7073
DevExpressExampleBot <fakeEmail>
2026-01-29 16:47:13 +0400

    Source auto update [skip ci]

--- VB/Solution/Data.vb
+++ VB/Solution/Data.vb
@@ -1,20 +1,59 @@
+﻿Imports System
 Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
 
 Namespace Solution
+	Public Class Data
+		Public Property ID() As Integer
+		Public Property Description() As String
+		Public Property ImageUrl() As String
+	End Class
 
-    Public Class Data
-
-        Public Property ID As Integer
-
-        Public Property Description As String
-
-        Public Property ImageUrl As String
-    End Class
-
-    Public Class DataHelper
-
-        Public Shared Function GenerateData() As List(Of Data)
-            Return New List(Of Data)() From {New Data() With {.ID = 0, .Description = "Basket", .ImageUrl = "Images/Basket.png"}, New Data() With {.ID = 1, .Description = "Customer", .ImageUrl = "Images/Customer.png"}, New Data() With {.ID = 2, .Description = "Home", .ImageUrl = "Images/Home.png"}, New Data() With {.ID = 3, .Description = "Mail", .ImageUrl = "Images/Mail.png"}, New Data() With {.ID = 4, .Description = "Check", .ImageUrl = "Images/Check.png"}, New Data() With {.ID = 5, .Description = "Folder", .ImageUrl = "Images/Folder.png"}, New Data() With {.ID = 6, .Description = "Key", .ImageUrl = "Images/Key.png"}, New Data() With {.ID = 7, .Description = "Printing", .ImageUrl = "Images/Printing.png"}}
-        End Function
-    End Class
+	Public Class DataHelper
+		Public Shared Function GenerateData() As List(Of Data)
+			Return New List(Of Data)() From {
+				New Data() With {
+					.ID = 0,
+					.Description = "Basket",
+					.ImageUrl = "Images/Basket.png"
+				},
+				New Data() With {
+					.ID = 1,
+					.Description = "Customer",
+					.ImageUrl = "Images/Customer.png"
+				},
+				New Data() With {
+					.ID = 2,
+					.Description = "Home",
+					.ImageUrl = "Images/Home.png"
+				},
+				New Data() With {
+					.ID = 3,
+					.Description = "Mail",
+					.ImageUrl = "Images/Mail.png"
+				},
+				New Data() With {
+					.ID = 4,
+					.Description = "Check",
+					.ImageUrl = "Images/Check.png"
+				},
+				New Data() With {
+					.ID = 5,
+					.Description = "Folder",
+					.ImageUrl = "Images/Folder.png"
+				},
+				New Data() With {
+					.ID = 6,
+					.Description = "Key",
+					.ImageUrl = "Images/Key.png"
+				},
+				New Data() With {
+					.ID = 7,
+					.Description = "Printing",
+					.ImageUrl = "Images/Printing.png"
+				}
+			}
+		End Function
+	End Class
 End Namespace
--- VB/Solution/Default.aspx.designer.vb
+++ VB/Solution/Default.aspx.designer.vb
@@ -6,44 +6,47 @@
 '     the code is regenerated. 
 ' </auto-generated>
 '------------------------------------------------------------------------------
+
 Namespace Solution
 
-    Public Partial Class [Default]
 
-        ''' <summary>
-        ''' form1 control.
-        ''' </summary>
-        ''' <remarks>
-        ''' Auto-generated field.
-        ''' To modify move field declaration from designer file to code-behind file.
-        ''' </remarks>
-        Protected form1 As Global.System.Web.UI.HtmlControls.HtmlForm
+	Partial Public Class [Default]
+		Inherits System.Web.UI.Page
 
-        ''' <summary>
-        ''' ASPxGridView1 control.
-        ''' </summary>
-        ''' <remarks>
-        ''' Auto-generated field.
-        ''' To modify move field declaration from designer file to code-behind file.
-        ''' </remarks>
-        Protected ASPxGridView1 As Global.DevExpress.Web.ASPxGridView
+		''' <summary>
+		''' form1 control.
+		''' </summary>
+		''' <remarks>
+		''' Auto-generated field.
+		''' To modify move field declaration from designer file to code-behind file.
+		''' </remarks>
+		Protected form1 As System.Web.UI.HtmlControls.HtmlForm
 
-        ''' <summary>
-        ''' ASPxPopupControl1 control.
-        ''' </summary>
-        ''' <remarks>
-        ''' Auto-generated field.
-        ''' To modify move field declaration from designer file to code-behind file.
-        ''' </remarks>
-        Protected ASPxPopupControl1 As Global.DevExpress.Web.ASPxPopupControl
+		''' <summary>
+		''' ASPxGridView1 control.
+		''' </summary>
+		''' <remarks>
+		''' Auto-generated field.
+		''' To modify move field declaration from designer file to code-behind file.
+		''' </remarks>
+		Protected ASPxGridView1 As Global.DevExpress.Web.ASPxGridView
 
-        ''' <summary>
-        ''' Image control.
-        ''' </summary>
-        ''' <remarks>
-        ''' Auto-generated field.
-        ''' To modify move field declaration from designer file to code-behind file.
-        ''' </remarks>
-        Protected Image As Global.DevExpress.Web.ASPxImage
-    End Class
+		''' <summary>
+		''' ASPxPopupControl1 control.
+		''' </summary>
+		''' <remarks>
+		''' Auto-generated field.
+		''' To modify move field declaration from designer file to code-behind file.
+		''' </remarks>
+		Protected ASPxPopupControl1 As Global.DevExpress.Web.ASPxPopupControl
+
+		''' <summary>
+		''' Image control.
+		''' </summary>
+		''' <remarks>
+		''' Auto-generated field.
+		''' To modify move field declaration from designer file to code-behind file.
+		''' </remarks>
+		Protected Image As Global.DevExpress.Web.ASPxImage
+	End Class
 End Namespace
