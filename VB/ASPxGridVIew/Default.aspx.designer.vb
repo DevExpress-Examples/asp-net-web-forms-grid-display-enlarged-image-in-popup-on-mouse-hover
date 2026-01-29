576e8024f9a0c996d88ad695e1057f8e57171649
DevExpressExampleBot <fakeEmail>
2026-01-29 16:48:10 +0400

    Source auto update [skip ci]

--- VB/ASPxGridVIew/Default.aspx.designer.vb
+++ VB/ASPxGridVIew/Default.aspx.designer.vb
@@ -6,49 +6,44 @@
 '     the code is regenerated. 
 ' </auto-generated>
 '------------------------------------------------------------------------------
-
-
-Imports Microsoft.VisualBasic
-Imports System
 Namespace ASPxGridVIew
 
+    Public Partial Class [Default]
 
-	Partial Public Class [Default]
+        ''' <summary>
+        ''' form1 control.
+        ''' </summary>
+        ''' <remarks>
+        ''' Auto-generated field.
+        ''' To modify move field declaration from designer file to code-behind file.
+        ''' </remarks>
+        Protected form1 As Global.System.Web.UI.HtmlControls.HtmlForm
 
-		''' <summary>
-		''' form1 control.
-		''' </summary>
-		''' <remarks>
-		''' Auto-generated field.
-		''' To modify move field declaration from designer file to code-behind file.
-		''' </remarks>
-		Protected form1 As Global.System.Web.UI.HtmlControls.HtmlForm
+        ''' <summary>
+        ''' ASPxGridView1 control.
+        ''' </summary>
+        ''' <remarks>
+        ''' Auto-generated field.
+        ''' To modify move field declaration from designer file to code-behind file.
+        ''' </remarks>
+        Protected ASPxGridView1 As Global.DevExpress.Web.ASPxGridView.ASPxGridView
 
-		''' <summary>
-		''' ASPxGridView1 control.
-		''' </summary>
-		''' <remarks>
-		''' Auto-generated field.
-		''' To modify move field declaration from designer file to code-behind file.
-		''' </remarks>
-		Protected ASPxGridView1 As Global.DevExpress.Web.ASPxGridView.ASPxGridView
+        ''' <summary>
+        ''' ASPxPopupControl1 control.
+        ''' </summary>
+        ''' <remarks>
+        ''' Auto-generated field.
+        ''' To modify move field declaration from designer file to code-behind file.
+        ''' </remarks>
+        Protected ASPxPopupControl1 As Global.DevExpress.Web.ASPxPopupControl.ASPxPopupControl
 
-		''' <summary>
-		''' ASPxPopupControl1 control.
-		''' </summary>
-		''' <remarks>
-		''' Auto-generated field.
-		''' To modify move field declaration from designer file to code-behind file.
-		''' </remarks>
-		Protected ASPxPopupControl1 As Global.DevExpress.Web.ASPxPopupControl.ASPxPopupControl
-
-		''' <summary>
-		''' Image control.
-		''' </summary>
-		''' <remarks>
-		''' Auto-generated field.
-		''' To modify move field declaration from designer file to code-behind file.
-		''' </remarks>
-		Protected Image As Global.DevExpress.Web.ASPxEditors.ASPxImage
-	End Class
+        ''' <summary>
+        ''' Image control.
+        ''' </summary>
+        ''' <remarks>
+        ''' Auto-generated field.
+        ''' To modify move field declaration from designer file to code-behind file.
+        ''' </remarks>
+        Protected Image As Global.DevExpress.Web.ASPxEditors.ASPxImage
+    End Class
 End Namespace
